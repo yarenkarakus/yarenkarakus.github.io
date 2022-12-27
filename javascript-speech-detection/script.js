2b87d57b143560ba5607c61a9186ef3388a89a0d
Yaren Karakus <yarenkarakus007@gmail.com>
2022-12-27 13:27:16 +0300

    JavaScript Speech Detection added

--- javascript-speech-detection/script.js
+++ javascript-speech-detection/script.js
@@ -10,6 +10,7 @@ words.appendChild(p);
 
 recognition.addEventListener("result", (e) => {
   console.log(e.results);
+
   const transcript = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
