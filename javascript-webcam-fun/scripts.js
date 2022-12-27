10a691306f880340fc6c48cfa6652af85cd71f65
Yaren Karakus <yarenkarakus007@gmail.com>
2022-12-27 12:13:19 +0300

    html and js fixed

--- javascript-webcam-fun/scripts.js
+++ javascript-webcam-fun/scripts.js
@@ -9,6 +9,14 @@ function getVideo() {
     .getUserMedia({ video: true, audio: false })
     .then((localMediaStream) => {
       console.log(localMediaStream);
+
+      //  DEPRECIATION :
+      //       The following has been depreceated by major browsers as of Chrome and Firefox.
+      //       video.src = window.URL.createObjectURL(localMediaStream);
+      //       Please refer to these:
+      //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+      //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
       video.srcObject = localMediaStream;
       video.play();
     })
@@ -17,7 +25,7 @@ function getVideo() {
     });
 }
 
-function paintToCanavas() {
+function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
@@ -25,34 +33,39 @@ function paintToCanavas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixel's out
+    // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
-    //pixels = redEffect(pixels);
-    //pixels = rgbSplit(pixels);
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
     // ctx.globalAlpha = 0.8;
-    pixels = greenScreen(pixels);
+
+    // pixels = greenScreen(pixels);
     // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
+
 function takePhoto() {
+  // played the sound
   snap.currentTime = 0;
   snap.play();
 
+  // take the data out of the canvas
   const data = canvas.toDataURL("image/jpeg");
   const link = document.createElement("a");
   link.href = data;
-  link.setAttribute("download", "beauty");
-  link.innerHTML = `<img src="${data}" alt="Beauty Girl" />`;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; //GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //BLUE
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
 }
@@ -60,14 +73,15 @@ function redEffect(pixels) {
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; //GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; //BLUE
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
   return pixels;
 }
 
 function greenScreen(pixels) {
   const levels = {};
+
   document.querySelectorAll(".rgb input").forEach((input) => {
     levels[input.name] = input.value;
   });
@@ -86,12 +100,14 @@ function greenScreen(pixels) {
       green <= levels.gmax &&
       blue <= levels.bmax
     ) {
+      // take it out!
       pixels.data[i + 3] = 0;
     }
   }
+
   return pixels;
 }
 
 getVideo();
 
-video.addEventListener("canplay", paintToCanavas);
+video.addEventListener("canplay", paintToCanvas);
